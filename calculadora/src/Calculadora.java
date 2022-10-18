bdcf6bd45145cfab3b4c4b065c6767ab804bd3f4
matheus <matheusmgs17@gmail.com>
2022-10-18 20:11:06 -0300

    smart Tv 2.0

---
+++ calculadora/src/Calculadora.java
@@ -0,0 +1,15 @@
+public class Calculadora {
+
+/* *
+* Este método é utilizado para  somar dois  números inteiros 
+* @param numeroUm este é o primeiro parâmetro  do método
+* @param numeroDois este é o segundo parâmetro  do método
+*@return int resultado deste método é a soma dos dois números.
+*/
+
+    public int somar(int numeroUm , int numeroDois){
+    return numeroUm + numeroDois;
+
+
+        }
+    }
