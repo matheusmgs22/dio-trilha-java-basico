d101f8ef70aae474ffb182eca19f462e3807ea22
matheus <matheusmgs17@gmail.com>
2022-10-21 09:39:23 -0300

    Operadores aula

---
+++ operadores.java/src/aulaOperadores.java
@@ -0,0 +1,36 @@
+public class aulaOperadores {
+
+
+    public static void main(String[] args) {
+
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 = 59.6d;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String s2 = "Fulano";
+        String s3 = "Fulano";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 1597L;
+        long l2 = 8997L;
+        byte y1 =1;
+        short h1 = 25;
+
+        System.out.println("i1 == i2" + (i1 == i2));
+        System.out.println("i1 != i2" + (i1 != i2));
+        System.out.println("i1 > i2" + (i1 > i2));
+        System.out.println("i1 <= i2" + (i1 <= i2));
+
+        
+        System.out.println("i1 == i2" + (i1 == i2));
+
+
+
+
+    }
+}
