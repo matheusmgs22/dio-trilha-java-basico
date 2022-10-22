5052d888e2b2073c9cbf6ab293a1c1cb0704e9f8
matheus <matheusmgs17@gmail.com>
2022-10-22 12:45:48 -0300

    exercicios de loop

---
+++ loop-e-array/src/parImpar.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+/*
+Faça um programa que peça N números inteiros,
+calcule e mostre a quantidade de números pares
+e a quantidade de números impares.
+*/
+
+public class parImpar {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros;
+        int numero;
+        int quantPares = 0;
+        int quantImpares = 0;
+
+        System.out.println("Quantidade de números: ");
+        quantNumeros = scan.nextInt();
+
+        int count = 0;
+        do {
+            System.out.println("Numero: ");
+            numero = scan.nextInt();
+            count++;
+
+            if(numero % 2 == 0){
+                quantPares++;
+
+            }else{
+
+                quantImpares++;
+            }
+
+        } while (count < quantNumeros);
+
+        System.out.println("Quantidade Par: " + quantPares);
+        System.out.println("Quantidade Impar: " + quantImpares);
+    }
+}
